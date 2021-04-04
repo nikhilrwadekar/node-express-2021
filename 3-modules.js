4d51a9368cff88de4495b3433161b721d5562b44
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2021-04-03 20:52:16 -0400

    File cleanup

---
+++ 3-modules.js
@@ -0,0 +1,17 @@
+// CommonJS - every file is a module.
+// (NodeJS uses this under the hood.)
+// Every file in node is a module by default.
+
+// Modules - Encapsulated Code. (Only share minimum)
+const names = require("./4-names.js");
+const utils = require("./5-utils.js");
+const { sayHi } = utils;
+const { john, peter } = names;
+sayHi(john);
+sayHi(peter);
+// sayHi(nikhil);
+
+const data = require("./6-alternative-flavor");
+console.log(data);
+
+require("./7-mind-grenade")
