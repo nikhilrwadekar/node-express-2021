3de7f9b8aea94a25792d3cd59ea6804a6c265c7f
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2021-04-03 21:54:24 -0400

    Done: Fs Module (Async)

---
+++ 11-fs-async.js
@@ -0,0 +1,34 @@
+const { readFile, writeFile } = require("fs");
+
+// Sync style. Err, Results. Always!
+readFile("./content/subfolder/first.txt", (err, result) => {
+  if (err) {
+    console.log("Sorry, could not load file.");
+  } else {
+    console.log("First.txt has been loaded.");
+    console.log("File contents:" + result);
+  }
+
+  const first = result;
+
+  readFile("./content/subfolder/second.txt", (err, result) => {
+    if (err) {
+      console.log("Sorry, could not load file.");
+    } else {
+      console.log("Second.txt has been loaded.");
+      console.log("File contents:" + result);
+
+      const second = result;
+        writeFile(
+            "./content/result-async/third.txt",
+            "Here is the third file!",
+            (err, result) => {
+                if(err) {
+                    console.log("Something went wrong with the callback hell.")
+                } else {
+                    console.log("Third file was created.")
+                }
+            });
+    }
+  });
+});
