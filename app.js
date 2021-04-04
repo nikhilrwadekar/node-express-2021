0dd6e2981845e1062e2676fd91df662a6fb3bc92
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2021-04-03 21:07:32 -0400

    I went on a tangent

--- app.js
+++ app.js
@@ -5,8 +5,36 @@ const freeRamInMB = os.freemem() / 1024 / 1024;
 console.log(`You have ${freeRamInMB} MB of RAM available.`);
 
 // Current uptime
-const systemUptimeInSeconds = os.uptime();
+const systemUptimeInSeconds = os.uptime(); // Avoiding this as this assigns one value and understandly so does not update the variable. (Isn't reactive, xD)
 
+// Helper display function
+const formatTimeAsString = (number, unit) => {
+    return `${number} ${unit}`;
+}
+
+// Print System uptime every second
 setInterval(() => {
-    console.log(os.uptime());
+    const uptimeSeconds = os.uptime();
+    const hours = parseInt(uptimeSeconds/60/60);
+    const minutes = parseInt(uptimeSeconds/60%60);
+    const seconds = uptimeSeconds%60;
+
+    let output = "";
+
+    if(hours) {
+        output += formatTimeAsString(hours, "hours");
+    }
+
+    if(minutes) {
+        output += " ";
+        output += formatTimeAsString(minutes, "minutes");
+    }
+
+    if(seconds) {
+        output += " ";
+        output += formatTimeAsString(seconds, "seconds");
+    }
+
+    console.log(output);
 }, 1000);
+
