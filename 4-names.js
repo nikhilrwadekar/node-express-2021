22b9dd896a265c36a13a6119605066bb84c75147
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2021-04-03 20:35:37 -0400

    Done: Modules

---
+++ 4-names.js
@@ -0,0 +1,16 @@
+// Shared
+const john = "John";
+const peter = "Peter";
+
+// Local
+const nikhil = "Nikhil";
+
+// Global
+// console.log(module);
+
+// So if you add your variables to module.exports it should be accessible throughout the app;
+
+module.exports = {
+    john,
+    peter
+};
--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-// Modules
+// CommonJS - every file is a module.
+// (NodeJS uses this under the hood.)
+// Every file in node is a module by default.
+
+// Modules - Encapsulated Code. (Only share minimum)
+const names = require("./4-names.js");
+const utils = require("./5-utils.js");
+const { sayHi } = utils;
+const { john, peter } = names;
+sayHi(john);
+sayHi(peter);
+// sayHi(nikhil);
